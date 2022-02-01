67589236d1634fcdda575226e238921db87fe879
Mike Worden <michael.worden@icloud.com>
2022-02-01 13:56:29 -0700

    Implemented FlickerURL

---
+++ Fotorama/FotoStore.swift
@@ -0,0 +1,15 @@
+//
+//  FotoStore.swift
+//  Fotorama
+//
+//  Created by Michael Worden on 2/1/22.
+//
+
+
+import Foundation
+
+class FotoStore {
+
+}
+
+
---
+++ Fotorama/FotosViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fotorama
+//
+//  Created by Michael Worden on 2/1/22.
+//
+
+import UIKit
+
+class FotosViewController: UIViewController {
+
+	
+	@IBOutlet private var imageView: UIImageView!
+	
+	
+	
+
+
+}
+
