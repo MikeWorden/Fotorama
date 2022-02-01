09b031e53b0d9170229c6a94af17b2a01b4968d7
Mike Worden <michael.worden@icloud.com>
2022-02-01 13:29:53 -0700

    Initial Commit

---
+++ Fotorama/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fotorama
+//
+//  Created by Michael Worden on 2/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
